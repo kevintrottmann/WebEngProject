69b469404429b8fd5bfda8fb313ecbb5f4b8fa34
kevin.trottmann <kevin.trottmann@students.fhnw.ch>
2017-10-14 14:49:48 +0200

    Login funktioniert mal grob

---
+++ pages/db.inc.php
@@ -0,0 +1,5 @@
+<?php
+$benutzer="db_root";
+$passwort="aVu8k&13";
+$dbname="db_photocase";
+?>
--- pages/pruef.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (@$_SESSION['eingeloggt']!='bb2017')
-{
-    echo "Sie haben keinen Zugriff hier <br/>";
-    echo "<a href='index.php'> Zum Login</a>";
-    exit();
-}
-    
-
