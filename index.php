0046a4d6b4a56f1a9e3799c707f45f5b40bb9ae7
matinski <32410868+matinski@users.noreply.github.com>
2017-11-05 18:04:15 +0100

    Umbau und Vorbereitung für einfachen Include von Funktionen
    Habe mit Haseeb das Index.php so vorbereitet, dass wir nur noch funktionien schreiben müssen, und uns den ganzen html-quatsch sparen können.

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+	$pageToInclude = './pages/welcome.php';
+	if(isset($_GET['page'])){	
+		$pageToInclude = './pages/' . $_GET['page'] . '.php';			
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
   
@@ -23,27 +29,13 @@
 	
 	
 	<body>
-		<!-- Static Navigation-Bar -->
-		<nav class="navigation navbar-inverse navbar-fixed-top">
-			<div class="container-fluid">
-			  	<div class="navbar-header">
-					<a href="https://www.photoca.se" class="navbar-brand"><img src="./pictures/logo/photocase_logo_small.png" alt="PhotoCase" style="auto"></a>
-				</div>
-			
-				<ul class="nav navbar-nav">
-					<li class="active"> <a href="https://www.photoca.se">Home</a></li>
-					<li><a href="/pages/prices.php">Preise</a></li>
-					<li><a href="/pages/booking.php">Buchen</a></li>
-					<li><a href="/pages/about.php">Über</a></li>
-					<li><a href="/pages/contact.php">Kontakt</a></li>
-				</ul>
-				
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="/pages/registrieren.php"><span class="glyphicon glyphicon-user"></span> Registrieren</a></li>
-					<li><a href="/pages/login.php"><span class="glyphicon glyphicon-log-in"></span> Anmelden</a></li>
-				</ul>
-			</div>
-		</nav>
-
+		<?php
+			include('./pages/navbar_new.php');			
+		?>
+		<div class="container-fluid">
+		<?php
+			include($pageToInclude);
+		?>
+		<div>
 	</body>
 </html>
