346295d1c830a152e26c930fce83f768bc6c3155
matinski <32410868+matinski@users.noreply.github.com>
2017-11-05 17:18:57 +0100

    Änderungen am index & home.php

--- index.php
+++ index.php
@@ -1,33 +1,49 @@
-<?php
-// Grabs the URI and breaks it apart in case we have querystring stuff
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
+<!DOCTYPE html>
+<html lang="en">
+  
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-// Route it up!
-switch ($request_uri[0]) {
-    // Home page
-    case '/':
-        require './pages/home.php';
-        break;
-	// Booking page
-    case './booking':
-        require '.photoca.se/pages/buchen.php';
-        break;
-	// Login page
-    case '/login':
-        require './pages/login.php';
-        break;	
-	// Register page
-    case '/register':
-        require './pages/register.php';
-        break;	
-    // About page
-    case '/about':
-        require './pages/about.php';
-        break;
-    // Everything else
-    default:
-        header('HTTP/1.0 404 Not Found');
-        require '../views/404.php';
-        break;
-}
-?>
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+		<!-- Stylesheets 
+		<link rel="stylesheet" href="./css/modal.css">	
+		-->	
+		
+	</head>
+	
+	
+	<body>
+		<!-- Static Navigation-Bar -->
+		<nav class="navigation navbar-inverse navbar-fixed-top">
+			<div class="container-fluid">
+			  	<div class="navbar-header">
+					<a href="https://www.photoca.se" class="navbar-brand"><img src="./pictures/logo/photocase_logo_small.png" alt="PhotoCase" style="auto"></a>
+				</div>
+			
+				<ul class="nav navbar-nav">
+					<li class="active"> <a href="https://www.photoca.se">Home</a></li>
+					<li><a href="/pages/prices.php">Preise</a></li>
+					<li><a href="/pages/booking.php">Buchen</a></li>
+					<li><a href="/pages/about.php">Über</a></li>
+					<li><a href="/pages/contact.php">Kontakt</a></li>
+				</ul>
+				
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/pages/registrieren.php"><span class="glyphicon glyphicon-user"></span> Registrieren</a></li>
+					<li><a href="/pages/login.php"><span class="glyphicon glyphicon-log-in"></span> Anmelden</a></li>
+				</ul>
+			</div>
+		</nav>
+
+	</body>
+</html>
