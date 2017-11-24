93c2cdf53bb729a245ff60b22fe4f58e345b08ad
kevin.trottmann <kevin.trottmann@students.fhnw.ch>
2017-11-24 16:13:26 +0100

    final

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?PHP
+session_start();
+
+// Session beenden 
+// damit können wir diese Seite als "Logout" verwenden
+session_unset();
+session_destroy();
+unset($_SESSION); // Session-Array löschen
+// Session-Cookie löschen 
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params["path"],
+        $params["domain"], $params["secure"], $params["httponly"]
+    );
+}
+?>
+
 
 <!DOCTYPE html>
 <html>
@@ -12,7 +29,13 @@
     <link rel="stylesheet" href="view/assets/css/styles.css">
 </head>
 
+
 <body style="background-color:rgb(97,100,102);">
+
+<?php
+// Session starten oder neu aufnehmen
+session_start();
+
 
     <div class="login-clean" style="background-color:rgb(97,100,102);">
         <form method="post">
--- view/registrieren.php
+++ view/registrieren.php
@@ -1,7 +1,3 @@
-<?php 
-session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=db_photocase', 'db_root', 'aVu8k&13');
-?>
 
 <!DOCTYPE html> 
 <html> 
@@ -12,63 +8,127 @@ $pdo = new PDO('mysql:host=localhost;dbname=db_photocase', 'db_root', 'aVu8k&13'
     <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
     <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
     <link rel="stylesheet" href="assets/css/styles.css">
+	
+	<script type="text/javascript">
+	function mySubmit() 
+	{
+	if(document.formular.benutzername.value == "") {
+    alert("Bitte tragen Sie Ihren Benutzername (Kontaktperson) ein!");
+    document.formular.benutzername.focus();
+    return false;
+    }
+	if(document.formular.passwort1.value == "") {
+    alert("Bitte geben sie ein Passwort ein!");
+    document.formular.passwort1.focus();
+    return false;
+    }
+	if(document.formular.passwort2.value == "") {
+    alert("Bitte geben sie das Passwort zweimal ein - zur Kontrolle!");
+    document.formular.passwort2.focus();
+    return false;
+    }
+    if(document.formular.passwort1.value.length < 8) {
+    alert("Bitte geben sie ein Passwort mit mindestens 8 Zeichen ein!")
+    document.formular.passwort1.focus();
+    return false;
+    }
+    var nr_length = document.formular.passwort1.value.replace(/[^0-9]/g, '').length;
+    if(nr_length < 1){
+    alert("Passwort muss mind. 1 Zahl haben !")
+    document.formular.passwort1.focus();
+    return false;
+    }    
+    if (document.formular.passwort1.value == document.formular.passwort2.value)
+      { 
+	  return true; 
+	  }
+	else
+	  {  
+	  alert("Passwörter sind nicht identisch");
+	  return false; 
+	  }
+	}
+    </script>
 </head>
 
+<body style="background-color:rgb(97,100,102);">
+
 <?php
-$showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
- 
-if(isset($_GET['register'])) {
- $error = false;
- $email = $_POST['email'];
- $passwort = $_POST['passwort'];
- $passwort2 = $_POST['passwort2'];
-  
- if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
- echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
- $error = true;
- } 
- if(strlen($passwort) == 0) {
- echo 'Bitte ein Passwort angeben<br>';
- $error = true;
- }
- if($passwort != $passwort2) {
- echo 'Die Passwörter müssen übereinstimmen<br>';
- $error = true;
- }
- 
- //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
- if(!$error) { 
- $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
- $result = $statement->execute(array('email' => $email));
- $user = $statement->fetch();
- 
- if($user !== false) {
- echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
- $error = true;
- } 
- }
- 
- //Keine Fehler, wir können den Nutzer registrieren
- if(!$error) { 
- 
- $statement = $pdo->prepare("INSERT INTO users (email, passwort) VALUES (:email, :passwort)");
- $result = $statement->execute(array('email' => $email, 'passwort' => $passwort));
- 
- if($result) { 
- echo'Weiterleiten zur Seite';
- $showFormular = false;
- } else {
- echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
- }
- } 
-}
- 
-if($showFormular) {
+// Session starten oder neu aufnehmen
+session_start();
+
+if (isset($_POST['erfassen']) OR isset($_POST['anpassen']))
+{
+		
+  if (isset($_POST['email']) AND isset($_POST['passwort1']))
+  {
+  $vonwo = $_POST["vonwo"];
+  $email = $_POST["email"];
+  $benutzername = $_POST["benutzername"];
+  $passwort1 = $_POST["passwort1"];
+  $passwort2 = $_POST["passwort2"];
+  $pass = md5($passwort1);
+
+    if (($passwort1 == $passwort2) && (strlen($passwort1) >= 8))
+    {
+    // Datenbankverbindung
+    include "db.inc.php";
+	$link=mysqli_connect("localhost", $benutzer, $passwort) or die("Keine Verbindung zur Datenbank!");
+    mysqli_select_db($link, $dbname) or die("Datenbank nicht gefunden!");
+
+    // damit ä,ö,ü und é richtig dargestellt werden! --> auf utf8 stellen
+    mysqli_query($link, "SET NAMES 'utf8'");
+	
+	// falls vom Formular anpassen 
+    if ($vonwo == "anpassen")      
+    { 
+      $anpassung = "UPDATE users SET `password`='$pass', `user_name`='$benutzername' WHERE `email`='$email'";
+      $angepasst = mysqli_query($link,$anpassung);
+      if ($angepasst == TRUE)
+	  {
+	    echo "Die Daten wurden angepasst<br/>";
+	    echo "Ihre Session_id ist:".session_id();
+        echo "<br/> <a href=\"login_c.php\">Zu den geheimen Daten</a>";
+	    echo "<br/> <a href=\"index.php\">Logout</a>";
+	    $_SESSION['name']=$email;
+        $_SESSION['eingeloggt']= true;
+	  }
+    } 
+	
+    // falls vom Formular "Neues Login" 
+    if ($vonwo == "erfassung")
+    {
+    // prüfen ob email bereits vorhanden
+    $abfrage="SELECT email FROM `users` WHERE email='$email'";
+	$ergebnis=mysqli_query($link,$abfrage) or die("Abfrage hat nicht geklappt!");
+	$count=mysqli_num_rows($ergebnis);
+	
+	if ($count == 1) 
+	  { echo "<font>Diese E-Mail-Adresse wurde bereits erfasst!</font>";}
+	else
+	  {
+	// Benutzer erfassen, weil noch nicht in DB vorhanden
+	$insert="INSERT INTO users (`user_id`, `user_name`, `password`, `email`) VALUES('','$benutzername','$pass','$email')";
+    mysqli_query($link,$insert)or die("DB-Eintrag hat nicht geklappt!");
+	echo "<font>Daten wurden erfasst!!</font><br/>";
+	  }
+	}
+    // Datenbankverbindung beenden
+    mysqli_close($link);  	    
+	} // end if passwörter gleich und länger als 8 Zeichen
+	else
+	{
+			echo "Passwörter waren nicht identisch";
+	} // end if passwörter gleich und länger als 8 Zeichen
+  } // end if isset email, passwort1
+}  // end if isset $submit
 ?>
 
-<body style="background-color:rgb(97,100,102);">
+
+
+
     <div class="login-clean" style="background-color:rgb(97,100,102);">
-        <form action="?register=1" method="post">
+        <form  method="post">
             <h2 class="sr-only">Login Form</h2>
             <div class="illustration">
                 <h1 style="color:rgb(244,164,71);">Registrierung</h1></div>
@@ -82,7 +142,7 @@ if($showFormular) {
                 <input class="form-control" type="email" name="email" placeholder="Email">
             </div>
             <div class="form-group">
-                <input class="form-control" type="passwort" name="passwort" placeholder="Passwort">
+                <input class="form-control" type="passwort" name="passwort1" placeholder="Passwort">
             </div>
 			<div class="form-group">
                 <input class="form-control" type="passwort" name="passwort2" placeholder="Passwort wiederholen">
@@ -95,9 +155,6 @@ if($showFormular) {
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
  
-<?php
-} //Ende von if($showFormular)
-?>
  
 </body>
 </html>
