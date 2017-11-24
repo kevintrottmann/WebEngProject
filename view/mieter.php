4cc2c198b2653a325fa062fb0af8de7f35b8572c
matinski <32410868+matinski@users.noreply.github.com>
2017-11-24 14:07:54 +0100

    Mieter Darstellung vollendet mit Buttons

--- view/mieter.php
+++ view/mieter.php
@@ -25,26 +25,25 @@ include "db.connection.php"; ?>
 						<th>Optionen</th>
                     </tr>
                 </thead>
+
                 <tbody>
-                        <?php
+                    <?php
                         $res_mieter=mysqli_query($link,"SELECT * FROM mieter");
-                        while ($datensatz=mysqli_fetch_assoc($res_mieter))
-                        {
+                        while ($datensatz=mysqli_fetch_assoc($res_mieter)){
 
-                        echo "<tr><td>".$datensatz["ID"]."</td>";
-                        echo "<td>".$datensatz["Nachname"]."</td>";
-                        echo "<td>".$datensatz["Vorname"]."</td>";
-                        echo "<td>".$datensatz["Strasse"]."</td>";
-                        echo "<td>".$datensatz["PLZ"]."</td>";
-                        echo "<td>".$datensatz["Ort"]."</td>";
-                        echo "<td>".$datensatz["Liegenschaft"]."</td>";
-                        echo "<td>".$datensatz["Mietzins"]."</td>";
-                        echo "<td>".$datensatz["Periode"]."</td>";
-                        echo "<td><a href=\"#\" class='btn btn-primary'> <span class='glyphicon glyphicon-pencil'></span></a></td>";
-      </p>;
+                            echo "<tr><td>".$datensatz["ID"]."</td>";
+                            echo "<td>".$datensatz["Nachname"]."</td>";
+                            echo "<td>".$datensatz["Vorname"]."</td>";
+                            echo "<td>".$datensatz["Strasse"]."</td>";
+                            echo "<td>".$datensatz["PLZ"]."</td>";
+                            echo "<td>".$datensatz["Ort"]."</td>";
+                            echo "<td>".$datensatz["Liegenschaft"]."</td>";
+                            echo "<td>".$datensatz["Mietzins"]."</td>";
+                            echo "<td>".$datensatz["Periode"]."</td>";
+                            echo "<td><a href=\"#\" class='btn btn-primary'> <span class='glyphicon glyphicon-pencil'></span></a>
+                                    <a href=\"#\" class='btn btn-primary'> <span class='glyphicon glyphicon-remove-circle'></span></a></td></tr>";
                         }
-                        ?>
-
+                    ?>
                 </tbody>
             </table>
 			<button class="btn btn-primary" type="button"> + Neuer Mieter </button>
