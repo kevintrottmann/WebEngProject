2d306c3164b7db0d93186c4a9a93ce041962d852
matinski <32410868+matinski@users.noreply.github.com>
2017-11-24 13:03:34 +0100

    Echo zu Abfragehinzugefügt

--- view/mieter.php
+++ view/mieter.php
@@ -28,22 +28,19 @@ include "db.connection.php"; ?>
                 <tbody>
                         <?php
                         $res_mieter=mysqli_query($link,"SELECT * FROM mieter");
-                        //while ($datensatz=mysqli_fetch_assoc($res_mieter))
+                        while ($datensatz=mysqli_fetch_assoc($res_mieter))
+                        {
 
-
-
-                        //{
-
-                        "<tr><td>".$datensatz["ID"]."</td>";
-                        "<td>".$datensatz["Nachname"]."</td>";
-                        "<td>".$datensatz["Vorname"]."</td>";
-                        "<td>".$datensatz["Strasse"]."</td>";
-                        "<td>".$datensatz["PLZ"]."</td>";
-                        "<td>".$datensatz["Ort"]."</td>";
-                        "<td>".$datensatz["Mietzins"]."</td>";
-                        "<td>".$datensatz["Periode"]."</td></tr>";
+                        echo "<tr><td>".$datensatz["ID"]."</td>";
+                        echo "<td>".$datensatz["Nachname"]."</td>";
+                        echo "<td>".$datensatz["Vorname"]."</td>";
+                        echo "<td>".$datensatz["Strasse"]."</td>";
+                        echo "<td>".$datensatz["PLZ"]."</td>";
+                        echo "<td>".$datensatz["Ort"]."</td>";
+                        echo "<td>".$datensatz["Mietzins"]."</td>";
+                        echo "<td>".$datensatz["Periode"]."</td></tr>";
                         //"<td>"<button class=\"btn btn-primary\" type=\"button\"> DEL </button><button class=\"btn btn-primary\" type=\"button\"> CHG </button>"</td></tr>";
-                        //}
+                        }
                         ?>
                 </tbody>
             </table>
@@ -57,14 +54,14 @@ include "db.connection.php"; ?>
             while ($datensatz=mysqli_fetch_assoc($res_mieter))
             {
                 echo "<tr><td>".$datensatz["ID"]."</td>";
-                echo "<td>".$datensatz["Nachname"]."</td>";
-                echo "<td>".$datensatz["Vorname"]."</td>";
-                echo "<td>".$datensatz["Strasse"]."</td>";
-                echo "<td>".$datensatz["PLZ"]."</td>";
-                echo "<td>".$datensatz["Ort"]."</td>";
-                echo "<td>".$datensatz["Mietzins"]."</td>";
-                echo "<td>".$datensatz["Periode"]."</td></tr>";
-                echo "<br />";
+            echo "<td>".$datensatz["Nachname"]."</td>";
+            echo "<td>".$datensatz["Vorname"]."</td>";
+            echo "<td>".$datensatz["Strasse"]."</td>";
+            echo "<td>".$datensatz["PLZ"]."</td>";
+            echo "<td>".$datensatz["Ort"]."</td>";
+            echo "<td>".$datensatz["Mietzins"]."</td>";
+            echo "<td>".$datensatz["Periode"]."</td></tr>";
+            echo "<br />";
             }
 
 					?>
