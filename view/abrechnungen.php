4e6cb91628ef3b7b8d78950a191ea1d4cfdb8e16
mikabeko <mikayil.bekooglu@gmail.com>
2017-11-28 20:14:35 +0100

    Edit Abrechnung and create pdfgenerator

--- view/abrechnungen.php
+++ view/abrechnungen.php
@@ -1,15 +1,57 @@
 <!DOCTYPE html>
 <html>
 
-<?php include "header.php"; 
+<?php include "header.php";
 include "db.connection.php"; ?>
- 
 
 <body>
     <div class="container">
         <div class="page-header">
-            <h3>Abrechnungen</h3></div>
-        
+            <h3>Abrechnungen</h3>
+            <a href="pdfgenerator.php" target="_blank" class="btn btn-primary">Drucken</a>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Mieter ID</th>
+                    <th>Vorname</th>
+                    <th>Nachname</th>
+                    <th>Strasse</th>
+                    <th>PLZ</th>
+                    <th>Ort</th>
+                    <th>Liegenschafts-Nr.</th>
+                    <th>Mietzins</th>
+                    <th>Periodizität</th>
+                    <th>Bearbeiten</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <?php
+                $res_mieter=mysqli_query($link,"SELECT * FROM mieter");
+                while ($datensatz=mysqli_fetch_assoc($res_mieter)){
+
+                    echo "<tr><td>".$datensatz["ID"]."</td>";
+                    echo "<td>".$datensatz["Nachname"]."</td>";
+                    echo "<td>".$datensatz["Vorname"]."</td>";
+                    echo "<td>".$datensatz["Strasse"]."</td>";
+                    echo "<td>".$datensatz["PLZ"]."</td>";
+                    echo "<td>".$datensatz["Ort"]."</td>";
+                    echo "<td>".$datensatz["Liegenschaft"]."</td>";
+                    echo "<td>".$datensatz["Mietzins"]."</td>";
+                    echo "<td>".$datensatz["Periode"]."</td>";
+                    //echo "<td><a href=\"#\" class='btn btn-primary'> <span class='glyphicon glyphicon-pencil'></span></a>
+                    //                <a href=\"#\" class='btn btn-primary'> <span class='glyphicon glyphicon-remove-circle'></span></a></td></tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+
+            </br>
+            </br>
+            </br>
+
         </div>
     </div>
 </body>
