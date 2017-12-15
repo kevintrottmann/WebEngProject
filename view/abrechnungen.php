641c61febbe74736a2f70b2e45c223877f7b66f2
kevin.trottmann <kevin.trottmann@students.fhnw.ch>
2017-12-15 13:58:46 +0100

    Abrechnung Vorbereitung

--- view/abrechnungen.php
+++ view/abrechnungen.php
@@ -15,39 +15,121 @@ include "db.connection.php"; ?>
 <div class="container">
     <div class="page-header">
         <h3>Abrechnungen</h3>
-        <a href="pdfgeneratorabrechnung.php" target="_blank" class="btn btn-primary">Drucken</a>
+		</br>
+		<div class="form-group">
+                <label class="control-label col-sm-2" for="id">Jahr der Abrechnung:</label>
+                <div class="col-sm-10">
+                   
+					<select name="jahr" style="height:40px;width:220px;font-size:20px;">
+				
+					<?php   echo '<option value=';
+							echo (date("Y")-1);
+							echo '>'; 
+							echo (date("Y")-1);
+							echo '</option>';
+							echo '<option value=';
+							echo date("Y");
+							echo '>'; 
+							echo date("Y");
+							echo '</option>';
+							echo '<option value=';
+							echo (date("Y")+1);
+							echo '>'; 
+							echo (date("Y")+1);
+							echo '</option>';
+							?>
+					
+
+					</select>
+				</div>
+            </div>
+			
+		</br>
+		</br>
+        <a href="pdfgeneratorabrechnung.php" target="_blank" class="btn btn-primary">Abrechnung Drucken</a>
+		<a href="pdfgeneratorheizkosten.php" target="_blank" class="btn btn-primary">Heizkosten Drucken</a>
+		<a href="pdfgeneratornebenkosten.php" target="_blank" class="btn btn-primary">Nebenkosten Drucken</a>
     </div>
     <div class="table-responsive">
+	<h3>Heizkosten</h3>
         <table class="table table-striped">
             <thead>
             <tr>
-                <th>Mieter ID</th>
-                <th>Vorname</th>
-                <th>Nachname</th>
-                <th>Strasse</th>
-                <th>PLZ</th>
-                <th>Ort</th>
-                <th>Liegenschafts-Nr.</th>
-                <th>Mietzins</th>
+                <th>ID</th>
+                <th>Typ</th>
+                <th>Art</th>
+                <th>Rechnungstext</th>
+                <th>Datum</th>
+                <th>Betrag</th>
             </tr>
             </thead>
 
             <tbody>
             <?php
-            $res_mieter=mysqli_query($link,"SELECT * FROM mieter");
-            while ($datensatz=mysqli_fetch_assoc($res_mieter)){
+            $res_rechnungenheiz=mysqli_query($link,"SELECT * FROM rechnungen WHERE Typ='Heizkosten'");
+            while ($datensatz=mysqli_fetch_assoc($res_rechnungenheiz)){
 
                 echo "<tr><td>".$datensatz["ID"]."</td>";
-                echo "<td>".$datensatz["Nachname"]."</td>";
-                echo "<td>".$datensatz["Vorname"]."</td>";
-                echo "<td>".$datensatz["Strasse"]."</td>";
-                echo "<td>".$datensatz["PLZ"]."</td>";
-                echo "<td>".$datensatz["Ort"]."</td>";
-                echo "<td>".$datensatz["Liegenschaft"]."</td>";
-                echo "<td>".$datensatz["Mietzins"]."</td>";
-                //echo "<td><a href=\"#\" class='btn btn-primary'> <span class='glyphicon glyphicon-pencil'></span></a>
-                //                <a href=\"#\" class='btn btn-primary'> <span class='glyphicon glyphicon-remove-circle'></span></a></td></tr>";
-            }
+                echo "<td>".$datensatz["Typ"]."</td>";
+                echo "<td>".$datensatz["Art"]."</td>";
+                echo "<td>".$datensatz["Rechnungstext"]."</td>";
+                echo "<td>".$datensatz["Datum"]."</td>";
+                echo "<td>".$datensatz["Betrag"]."</td></tr>";
+				}
+			$res_resultatheiz=mysqli_query($link,"SELECT SUM(Betrag) FROM rechnungen WHERE Typ='Heizkosten'");
+            while ($datensatz1=mysqli_fetch_assoc($res_resultatheiz)){
+					echo "<td></td>";
+					echo "<td></td>";
+					echo "<td></td>";
+					echo "<td><h3>Total:</h3></td>";
+					echo "<td></td>";
+					echo "<td><h3>".$datensatz1["SUM(Betrag)"]."</h3></td>";
+										
+                 }
+            ?>
+            </tbody>
+        </table>
+
+        </br>
+        </br>
+        </br>
+
+    
+	<h3>Nebenkosten</h3>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Typ</th>
+                <th>Art</th>
+                <th>Rechnungstext</th>
+                <th>Datum</th>
+                <th>Betrag</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <?php
+            $res_rechnungenneben=mysqli_query($link,"SELECT * FROM rechnungen WHERE Typ='Nebenkosten'");
+            while ($datensatz2=mysqli_fetch_assoc($res_rechnungenneben)){
+
+                echo "<tr><td>".$datensatz2["ID"]."</td>";
+                echo "<td>".$datensatz2["Typ"]."</td>";
+                echo "<td>".$datensatz2["Art"]."</td>";
+                echo "<td>".$datensatz2["Rechnungstext"]."</td>";
+                echo "<td>".$datensatz2["Datum"]."</td>";
+                echo "<td>".$datensatz2["Betrag"]."</td></tr>";
+				}
+			$res_resultatneben=mysqli_query($link,"SELECT SUM(Betrag) FROM rechnungen WHERE Typ='Nebenkosten'");
+            while ($datensatz3=mysqli_fetch_assoc($res_resultatneben)){
+					echo "<td></td>";
+					echo "<td></td>";
+					echo "<td></td>";
+					echo "<td><h3>Total:</h3></td>";
+					echo "<td></td>";
+					echo "<td><h3>".$datensatz3["SUM(Betrag)"]."</h3></td>";
+										
+                 }
             ?>
             </tbody>
         </table>
