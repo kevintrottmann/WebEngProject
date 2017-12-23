bf5aecf346cbb9ed5ff94f08628106156ba597fd
mikabeko <mikayil.bekooglu@gmail.com>
2017-12-23 15:23:26 +0100

    changed Logout

--- view/logout.php
+++ view/logout.php
@@ -1,18 +1,12 @@
 <?php
 session_start();
-
-$_SESSION = array();
-
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
+session_unset();
+session_destroy();
+if (isset($_COOKIE['PHPSESSID'])) {
+    unset($_COOKIE['PHPSESSID']);
+    setcookie('PHPSESSID', '', time() - 3600, '/'); // empty value and old timestamp
 }
 
-session_destroy();
+header("location:http://photoca.se/index.php");
+?>
 
-header("Location: http://photoca.se/index.php");
-die();
-?> 
