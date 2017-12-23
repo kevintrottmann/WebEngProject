b55352c8d618ce97e0e7e16c4816ab52e97f4b44
matinski <32410868+matinski@users.noreply.github.com>
2017-12-23 12:20:12 +0100

    alte doks gelöscht

--- altePage/pages/db.connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost";
-$benutzer="db_root";
-$passwort="aVu8k&13";
-$dbname="db_photocase";
-
-$link=mysqli_connect($host,$benutzer,$passwort,$dbname) or die
-("Keine Verbindung zur DB moeglich");
-?>
