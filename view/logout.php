9c827a726e2aacc5ebf10f1ad42def55c970a92f
matinski <32410868+matinski@users.noreply.github.com>
2017-12-23 12:43:27 +0100

    Logout

--- view/logout.php
+++ view/logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+$_SESSION = array();
+
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
